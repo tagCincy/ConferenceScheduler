033db8e15cae2195fc0617b9670d08486ece5e4d
Tim Guibord <timguibord@gmail.com>
2014-02-16 16:33:30 -0500

    Updated User Model

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,21 @@ namespace ConferenceScheduler.Models
 {
     public class User
     {
+        [Key]
         public int UserID { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
+        [Required]
         public string Company { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
